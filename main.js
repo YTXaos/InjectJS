3bd59c8cf2298b552853460c97c626e33becca0e
YTXaos.Alonso <alonso.alegria.olguin@gmail.com>
2022-11-30 16:29:53 -0500

    Update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         InjectJS
 // @namespace    http://tampermonkey.net/
-// @version      1.02
+// @version      1.03
 // @description  Inject javascript into almost every website you visit.
 // @author       You
 // @match        *://*/*
@@ -17,7 +17,6 @@
     const options = JSON.parse(localStorage.getItem("injectjs-options")), popup = document.createElement("div"), style = document.createElement("style");
     fetch("https://raw.githubusercontent.com/YTXaos/InjectJS/main/assets/main.css").then(get => get.text()).then(set => style.innerHTML = set);
     popup.setAttribute("class", "js-injector-popup");
-    popup.setAttribute("style", "display: none;");
     popup.innerHTML = `
     <label class="js-inject-header">
     <div class="js-logo-needle">.....</div>
