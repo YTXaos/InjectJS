1b463880e3b75b026b8fd2229743a370fae6e433
Alonso <69170946+YTXaos@users.noreply.github.com>
2022-12-06 17:12:46 +0000

    Multi-language description

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 // ==UserScript==
-// @name         InjectJS
-// @namespace    http://github.com/YTXaos/InjectJS
-// @version      1.22
-// @description  Inject javascript into almost every website you visit.
-// @author       YTXaos
-// @match        *://*/*
-// @match        file:///*
-// @icon         https://raw.githubusercontent.com/YTXaos/InjectJS/main/assets/logo.png
-// @grant        GM_addElement
-// @license      MIT
-// @downloadURL  https://raw.githubusercontent.com/YTXaos/InjectJS/main/main.js
-// @updateURL    https://raw.githubusercontent.com/YTXaos/InjectJS/main/main.js
-// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @name             InjectJS
+// @namespace        http://github.com/YTXaos/InjectJS
+// @version          1.22
+// @description      Inject Javascript into almost any website.
+// @description:es   Inyecte Javascript en casi cualquier sitio web
+// @description:fr   Injectez Javascript dans presque tous les sites Web
+// @description:de   Fügen Sie Javascript in fast jede Website ein
+// @description:ja   ほぼすべての Web サイトに ジャバスクリと を挿入する
+// @description:la   Javascript in inject paene omnem website
+// @description:ru   Внедрите Javascript практически в любой веб-сайт
+// @author           YTXaos
+// @match            *://*/*
+// @match            file:///*
+// @icon             https://raw.githubusercontent.com/YTXaos/InjectJS/main/assets/logo.png
+// @grant            GM_addElement
+// @license          MIT
+// @downloadURL      https://raw.githubusercontent.com/YTXaos/InjectJS/main/main.js
+// @updateURL        https://raw.githubusercontent.com/YTXaos/InjectJS/main/main.js
+// @require          https://code.jquery.com/jquery-3.6.0.min.js
 // ==/UserScript==
 
 (function() {
